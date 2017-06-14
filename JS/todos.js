1a8b64e9ecfa79d56668c5c9edb19d42c66ba2c9
Jonathan O <jonathanotano@icloud.com>
2017-06-14 08:26:55 -0400

    added files
    added index, css, and js

---
+++ JS/todos.js
@@ -0,0 +1,29 @@
+// check off specific todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+
+// click on X to delete todo
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// add new to-do with enter
+$("#input").keypress(function (event){
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+		// delete text input
+		$(this).val("");
+	}
+});
+
+// toggle plus sign
+$(".fa-plus").click(function(){
+	$("#input").slideToggle();
+})
